b6b4b0f5d5d6a2f61fbf5c5baa7c898bb9011218
Eugene Schwartz <nonsenseftw@yandex.ru>
2017-05-09 17:52:05 +0500

    init

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,11 @@ class Post < ApplicationRecord
   include ImageUploader::Attachment.new(:image)
 
   validates :description, presence: true
+
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
+  def should_generate_new_friendly_id?
+    title_changed?
+  end
 end
