4aa0a46c67baaf354c17cd23eefb6cfee858834e
Eugene Schwartz <nonsenseftw@yandex.ru>
2017-06-23 21:46:55 +0500

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,23 @@
 Rails.application.routes.draw do
-  root to: 'posts#index'
-  get ':user_name', to: 'profiles#show', as: :profile
-  get ':user_name/edit', to: 'profiles#edit', as: :edit_profile
-  patch ':user_name/edit', to: 'profiles#update', as: :update_profile
-
-  resources :posts do
-    resources :comments
+  devise_scope :user do
+    get '/register', to: 'devise/registrations#new', as: 'new_user_registration' # custom path to sign_up/registration
   end
 
   devise_for :users, controllers: {
     registrations: 'users/registrations'
-  }
+  }, skip: [:sessions]
+  as :user do
+    get 'login', to: 'devise/sessions#new', as: :new_user_session
+    post 'signin', to: 'devise/sessions#create', as: :user_session
+    delete 'signout', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
+
+  root to: 'posts#index'
+  get ':user_name', to: 'profiles#show', as: :profile
+  get ':user_name/edit', to: 'profiles#edit', as: :edit_profile
+  patch ':user_name/edit', to: 'profiles#update', as: :update_profile
+  # get '/new', to: 'posts#new', as: :new_post
+  resources :posts do
+    resources :comments
+  end
 end
