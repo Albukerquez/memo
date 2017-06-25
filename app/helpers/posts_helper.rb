51d0c1c4d1ee72e2fea56352491607ed01cc06a6
Eugene Schwartz <nonsenseftw@yandex.ru>
2017-06-26 00:30:10 +0500

    fff

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,10 +1,21 @@
 module PostsHelper
   # def display_likers(post)
   #   likes = post.likes.all
-  #   return count_likers(likes) if likes.size <= 8
+  #   return list_likers(likes) if likes.size <= 8
+  #   count_likers(likes)
   # end
   #
   # private
+  #
+  # def list_likers
+  #   user_names = []
+  #   if post.like?.each do |liker|
+  #       user_names.push(link_to liker.user_name, profile_path(user.user_name),
+  #       class: 'user-name')
+  #   end
+  #     user_names.to_sentence.html_safe
+  #   end
+  # end
   #
   # def count_likers(likes)
   #   like_count = likes.size
