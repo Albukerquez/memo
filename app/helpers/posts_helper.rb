8f6caddcea37a9378662f9c7068856aab190ca09
Eugene Schwartz <nonsenseftw@yandex.ru>
2017-06-25 20:42:34 +0500

    likes

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,13 @@
 module PostsHelper
+  # def display_likers(post)
+  #   likes = post.likes.all
+  #   return count_likers(likes) if likes.size <= 8
+  # end
+  #
+  # private
+  #
+  # def count_likers(likes)
+  #   like_count = likes.size
+  #   like_count.to_s + ' likes'
+  # end
 end
